e6bdacfc751f7dfee61d84fb06689c432d409f56
BorislavMatveev <borislav.matveev.2000@bk.ru>
2025-11-01 23:03:43 +0300

    writing main

--- 01.11.25/main.cpp
+++ 01.11.25/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+int**make(int rows, int cors);
+void output(const int *const *mtx);
+void rm(int **mtx);
+
 
 int main()
 {
+int rows=0,cols=0;
+std::cin >> rows >> cols;
+if(std::cin.fail())
+{
+  return 1;
+}
 
+int **mtx=nullptr;
+output(mtx);
+rm(mtx);
 return 0;
 }
