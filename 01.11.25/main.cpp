1290d7cc7773e86ec9b9481c09a5548b6be912f9
BorislavMatveev <borislav.matveev.2000@bk.ru>
2025-11-01 23:24:11 +0300

    creation functions

--- 01.11.25/main.cpp
+++ 01.11.25/main.cpp
@@ -3,18 +3,45 @@ int**make(int rows, int cors);
 void output(const int *const *mtx);
 void rm(int **mtx);
 
-
 int main()
 {
-int rows=0,cols=0;
-std::cin >> rows >> cols;
-if(std::cin.fail())
-{
-  return 1;
+  int rows=0,cols=0;
+  std::cin >> rows >> cols;
+  if(std::cin.fail())
+  {
+    return 1;
+  }
+
+  int **mtx=nullptr;
+  try{
+  mtx = make(rows, cols);
+  }
+  catch(const std::bad_alloc){
+  return 2;
+  }
+  output(mtx);
+  rm(mtx);
+  return 0;
 }
 
-int **mtx=nullptr;
-output(mtx);
-rm(mtx);
-return 0;
+void rm(int **mtx, int rows)
+{
+  for (size_t i = 0; i < rows; i++)
+  {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
+}
+
+int **make (int rows, int cors){
+  int **mtx = new int*[rows];
+  for (size_t i=0; i<rows; ++i)
+  { try{
+      mtx[i]=new int[cors];
+    }
+    catch(...){
+      rm(mtx,i);
+    }
+  }
+  return mtx;
 }
