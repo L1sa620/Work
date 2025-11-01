fc4c704ba57b0d87120c751853b59f54c67cccb8
BorislavMatveev <borislav.matveev.2000@bk.ru>
2025-11-02 00:29:43 +0300

    function convert

--- 01.11.25/homework.cpp
+++ 01.11.25/homework.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 int**make(int rows, int cors);
-void output(const int *const *mtx, int rows, int cors);
+void output(const int *const *mtx, int rows, size_t *lns);
 void rm(int **mtx, int rows);
 void input(int ** mtx, int rows, int cors);
+int** convert(const int *t, size_t n, const size_t *lns, size_t rows);
 int main()
 {
-  int rows=0,cols=0;
-  std::cin >> rows >> cols;
+  int rows=0, n=0;
+  std::cin >> n;
   if(std::cin.fail())
   {
     return 1;
   }
-  int **mtx=nullptr;
-  try{
-  mtx = make(rows, cols);
+  int *t = new int[n];
+  for(int i = 0; i < n; ++i) {
+    std::cin >> t[i];
   }
-  catch(const std::bad_alloc){
-  return 2;
+  std::cin >> rows;
+  if(std::cin.fail())
+  {
+    delete[] t;
+    return 1;
   }
-  input(mtx, rows, cols);
-  output(mtx, rows, cols);
+  size_t *lns = new size_t[rows];
+  for(int i = 0; i < rows; ++i) {
+    std::cin >> lns[i];
+  }
+  int **mtx = convert(t, n, lns, rows);
+  output(mtx, rows, lns);
   rm(mtx, rows);
+  delete[] t;
+  delete[] lns;
   return 0;
 }
 
@@ -55,12 +65,24 @@ void input(int **mtx, int rows, int cols){
   }
 }
 
-void output(const int *const *mtx, int rows, int cols){
+void output(const int *const *mtx, int rows, size_t *lns){
+  std::cout << "Out\n";
   for(size_t i=0; i<rows; ++i){
-    for(size_t j=0; j<cols; ++j){
+    for(size_t j=0; j<lns[i]; ++j){
       std::cout << mtx[i][j] << " ";
     }
     std::cout << "\n";
   }
 }
 
+int** convert(const int *t, size_t n, const size_t *lns, size_t rows) {
+  int **mtx = new int*[rows];
+  size_t index=0;
+  for (size_t i=0; i<rows; ++i) {
+    mtx[i] = new int[lns[i]];
+    for (size_t j=0; j<lns[i]; ++j) {
+      mtx[i][j] = t[index++];
+    }
+  }
+  return mtx;
+}
